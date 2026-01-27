f3bb866e10c463df2cf51b2a55b44676d86dc1d3
coco <maxinrui@echo.tech>
2026-01-27 14:52:29 +0800

    up

--- library/lpw_stable_diffusion.py
+++ library/lpw_stable_diffusion.py
@@ -9,7 +9,11 @@ import numpy as np
 import PIL.Image
 import torch
 from packaging import version
-from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer, CLIPVisionModelWithProjection
+try:
+    from transformers import CLIPFeatureExtractor
+except ImportError:  # transformers>=4.41 removes CLIPFeatureExtractor
+    from transformers import CLIPImageProcessor as CLIPFeatureExtractor
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPVisionModelWithProjection
 
 import diffusers
 from diffusers import SchedulerMixin, StableDiffusionPipeline
